4c71a4b49df994c77bebe013ddb5481073bb02e0
Gabriel Gonzalez <Gabriel439@gmail.com>
2018-02-25 10:16:59 -0800

    Improve clarity of example

--- src/Sig.hs
+++ src/Sig.hs
@@ -64,8 +64,6 @@ import qualified Foreign.Marshal.Unsafe
 -- * `S02` - In the middle of a block comment
 -- * `S03` - Just parsed a @\'*\'@ that might be the first character in @\"*/\"@
 --
--- > {-# LANGUAGE RecordWildCards #-}
--- > 
 -- > import Sig (State(..), StateMachine)
 -- > 
 -- > import qualified Sig
@@ -74,24 +72,23 @@ import qualified Foreign.Marshal.Unsafe
 -- > cStyleComments = Sig.buildStateMachine f
 -- >   where
 -- >     -- 47 is the ASCII encoding for '/'
--- >     f 47 S00 = S01  -- Possible comment start: Go to state #1
--- >     f 47 S01 = S01
--- >     f 47 S02 = S02
--- >     f 47 S03 = S00  -- Confirmed comment end: Go to state #0
--- >
 -- >     -- 42 is the ASCII encoding for '*'
--- >     f 42 S00 = S00
--- >     f 42 S01 = S02  -- Confirmed comment start: Go to state #2
--- >     f 42 S02 = S03  -- Possible comment end: Go to state #3
--- >     f 42 S03 = S03
 -- >
--- >     -- This covers all other ASCII characters
--- >     f  _ S00 = S00  -- Still outside a comment: Stay on state #0
--- >     f  _ S01 = S00
--- >     f  _ S02 = S02  -- Still inside a comment: Stay on state #2
--- >     f  _ S03 = S02
--- > 
--- >     -- This covers all other states (which we don't use)
+-- >     f 47 S00 = S01  -- Possible  comment start: Go to state #1
+-- >     f 42 S01 = S02  -- Confirmed comment start: Go to state #2
+-- >     f 42 S02 = S03  -- Possible  comment end  : Go to state #3
+-- >     f 47 S03 = S00  -- Confirmed comment end  : Go to state #0
+-- >
+-- >     f 47 S01 = S01  -- Still might be a comment start: Stay on   state #1
+-- >     f  _ S01 = S00  -- Not a comment after all       : Return to state #0
+-- >
+-- >     f 42 S03 = S03  -- Still might be a comment end  : Stay on   state #3
+-- >     f  _ S03 = S02  -- Not a comment after all       : Return to state #2
+-- >
+-- >     f  _ S00 = S00  -- Outside of a comment: Stay on state #0
+-- >
+-- >     f  _ S02 = S02  -- Inside a comment    : Stay on state #2
+-- >
 -- >     f  _ _   = S00
 --
 -- ... and here is an example of using the above `StateMachine` on a file:
--- src/Sig/Examples.hs
+++ src/Sig/Examples.hs
@@ -11,22 +11,23 @@ import qualified Sig
 cStyleComments :: StateMachine
 cStyleComments = Sig.buildStateMachine f
   where
-    -- '*'
-    f 42 S00 = S00
-    f 42 S01 = S02
-    f 42 S02 = S03
-    f 42 S03 = S03
+    -- 47 is the ASCII encoding for '/'
+    -- 42 is the ASCII encoding for '*'
 
-    -- '/'
-    f 47 S00 = S01
-    f 47 S01 = S01
-    f 47 S02 = S02
-    f 47 S03 = S00
+    f 47 S00 = S01  -- Possible  comment start: Go to state #1
+    f 42 S01 = S02  -- Confirmed comment start: Go to state #2
+    f 42 S02 = S03  -- Possible  comment end  : Go to state #3
+    f 47 S03 = S00  -- Confirmed comment end  : Go to state #0
 
-    f  _ S00 = S00
-    f  _ S01 = S00
-    f  _ S02 = S02
-    f  _ S03 = S02
+    f 47 S01 = S01  -- Still might be a comment start: Stay on   state #1
+    f  _ S01 = S00  -- Not a comment after all       : Return to state #0
+
+    f 42 S03 = S03  -- Still might be a comment end  : Stay on   state #3
+    f  _ S03 = S02  -- Not a comment after all       : Return to state #2
+
+    f  _ S00 = S00  -- Outside of a comment: Stay on state #0
+
+    f  _ S02 = S02  -- Inside a comment    : Stay on state #2
 
     f  _ _   = S00
 
